52f24938cdc9d3a3344c2dd3f83f6d2527e15400
XDdevv <undefineduser087@gmail.com>
2025-05-26 21:36:03 +0500

    Initial commit

---
+++ app/src/main/java/zed/rainxch/plmastermememvi/MainActivity.kt
@@ -0,0 +1,26 @@
+package zed.rainxch.plmastermememvi
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import zed.rainxch.plmastermememvi.ui.theme.PLMasterMemeMVITheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PLMasterMemeMVITheme {
+                Test()
+            }
+        }
+    }
+}
